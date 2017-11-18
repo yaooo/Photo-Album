336bdcd4467179ae0f9a33432c8ed173c4077f55
yao <yaoshi616@gmail.com>
2017-11-18 17:17:27 -0500

    Start working on photolist and its display

--- src/controller/AlbumController.java
+++ src/controller/AlbumController.java
@@ -14,7 +14,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
-import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
---
+++ src/controller/PhotoListController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class PhotoListController {
+    @FXML
+    private Text albumTitle1;
+
+    private Button copyPhoto1;
+
+    private Button movePhoto1;
+
+    private Button addPhoto1;
+
+    private Button renamePhoto1;
+
+    private Button photoTag1;
+
+    private Button slideShow1;
+
+    private Button exit1;
+}
