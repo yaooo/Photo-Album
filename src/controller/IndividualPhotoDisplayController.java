d15c56a7696bc589a00d31aa2f72e3226f4b8c02
yao <yaoshi616@gmail.com>
2017-11-19 22:23:44 -0500

    Display almost done. Still need to do tags.

--- src/controller/IndividualPhotoDisplayController.java
+++ src/controller/IndividualPhotoDisplayController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import model.Tag;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -8,7 +9,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 import model.Album;
 import model.Photo;
@@ -16,8 +20,10 @@ import model.User;
 import model.UserList;
 
 import javafx.event.ActionEvent;
+
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class IndividualPhotoDisplayController {
@@ -31,20 +37,44 @@ public class IndividualPhotoDisplayController {
     @FXML
     private Button IndividualDone;
 
+    @FXML
+    private TextArea IndividualCaption;
+
+    @FXML
+    private Text DateOfPhoto;
+
     private User currentUsername;
     private Album album;
+    private Photo currentPhoto;
+
     public void start(Stage Stage, Photo photo, User username, Album a) throws ClassNotFoundException, IOException {
 //        PhotoDisplay = new ImageView();
         album = a;
         currentUsername = username;
+        currentPhoto = photo;
         PhotoDisplay.setImage(photo.getImage());
+
+        DisplayCaption();
+        DisplayDateTime();
     }
 
     private void DisplayTags(){
         //TODO
-
     }
 
+    private void DisplayCaption(){
+        String c = currentPhoto.getCaption();
+        IndividualCaption.setEditable(false);
+
+        IndividualCaption.setWrapText(true);     // New line of the text exceeds the text area
+        IndividualCaption.setPrefRowCount(10);
+        IndividualCaption.setText(c);
+    }
+
+    private void DisplayDateTime(){
+        String time = currentPhoto.getDate();
+        DateOfPhoto.setText(time);
+    }
 
     @FXML
     protected void handleDoneButton(ActionEvent event) throws IOException,  ClassNotFoundException {
